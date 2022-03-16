f1a80598d3fddad56e02cc9867cad876ca8b488c
zhj <1515112102@qq.com>
2022-03-16 14:44:40 +0800

    update dev-derver

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   devtool: 'inline-source-map',
   devServer: {
-    static: './dist',
+    contentBase: './dist',
   },
   plugins: [new HtmlWebpackPlugin({
     title: "My App",
